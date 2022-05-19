260ba7e96203831aaeefa777e77e2c17600009e5
Alice <alicelu625@gmail.com>
2022-05-18 23:27:34 -0700

    added notes for reducers

--- react-advanced-effects-reducers-context/src/components/Login/Login.js
+++ react-advanced-effects-reducers-context/src/components/Login/Login.js
@@ -12,49 +12,70 @@ import AuthContext from '../../store/auth-context';
 import Input from '../UI/Input/Input';
 import classes from './Login.module.css';
 
-//email validation
+/****************** Reducer functions ******************/
+//can define reducer function outside of component function because it does not need to interact with anything inside
+//email value & validation
 const emailReducer = (state, action) => {
+  //email input changes
   if (action.type === 'USER_INPUT') {
+    //value from payload & check validity
     return { value: action.val, isValid: action.val.includes('@') };
   }
+  //input field blurred (loses focus)
   if (action.type === 'INPUT_BLUR') {
+    //value in state & check validity
     return { value: state.value, isValid: state.value.includes('@') };
   }
   return { value: '', isValid: false };
 };
 
-//password validation
+//password value & validation
 const passwordReducer = (state, action) => {
+  //password input changes
   if (action.type === 'USER_INPUT') {
+    //value from payload & check validity
     return { value: action.val, isValid: action.val.trim().length > 6 };
   }
+  //input field blurred (loses focus)
   if (action.type === 'INPUT_BLUR') {
+    //value in state & check validity
     return { value: state.value, isValid: state.value.trim().length > 6 };
   }
   return { value: '', isValid: false };
 };
 
+
+/****************** Component ******************/
 const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
+  /*replaced with use of Reducers
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  */
   const [formIsValid, setFormIsValid] = useState(false);
 
+
+  /****************** Reducers ******************/
+  //reducer for email
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
     isValid: null,
   });
+
+  //reducer for password
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
     isValid: null,
   });
 
+  
   const authCtx = useContext(AuthContext);
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
+  /* Test function
   //test side effect function
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -64,7 +85,11 @@ const Login = (props) => {
       console.log('EFFECT CLEANUP');
     };
   }, []);
+  */
 
+
+  /****************** Side effect function to check validity ******************/
+  //use object destructuring to pull the isValid state into a constant (emailIsValid)
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
@@ -73,6 +98,7 @@ const Login = (props) => {
     //check validity when user pauses typing for 5s (so it's not ran every keystroke)
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
+      //update state based on other states
       setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
 
@@ -85,8 +111,11 @@ const Login = (props) => {
     };
   }, [emailIsValid, passwordIsValid]);
 
+
+  /****************** Input change handlers ******************/
   //when email input changes
   const emailChangeHandler = (event) => {
+    //dispatch function containing object with action identifier (type) & payload (value user entered)
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
 
     // setFormIsValid(
@@ -96,19 +125,27 @@ const Login = (props) => {
 
   //when password input changes
   const passwordChangeHandler = (event) => {
+    //dispatch function containing object with action identifier (type) & payload (value user entered)
     dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
 
     // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
+
+  /****************** Input blur handlers ******************/
+  //when email input field blurs (loses focus, user clicks out)
   const validateEmailHandler = () => {
+    //dispatch object
     dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
+  //when password input field blurs (loses focus, user clicks out)
   const validatePasswordHandler = () => {
+    //dispatch object
     dispatchPassword({ type: 'INPUT_BLUR' });
   };
 
+  /****************** Submit handler ******************/
   const submitHandler = (event) => {
     event.preventDefault();
     if (formIsValid) {
